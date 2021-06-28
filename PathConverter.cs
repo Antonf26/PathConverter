0529d5c8447a7af7a376bc62819bc02fc555a5fe
Antonf26 <antonf26@gmail.com>
2021-06-28 07:58:28 +0100

    Add files via upload

---
+++ PathConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace JsonPathConvert
+{
+    public static class PathConverter
+    {
+        public static string Convert(string input)
+        {
+            var replace = Regex.Replace(input, @"\.(\w+ \w+)($|\[|\.| )", ".['$1']$2");
+            return replace;
+        }
+    }
+}
